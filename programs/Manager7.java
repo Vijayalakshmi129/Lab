b1a73749b48346ebdfa45e7dd0b2a4b57df81d47
Vijayalakshmi129 <87688226+Vijayalakshmi129@users.noreply.github.com>
2022-09-23 12:30:14 +0530

    Add files via upload

---
+++ programs/Manager7.java
@@ -0,0 +1,47 @@
+import java.util.Comparator;
+import java.util.TreeMap;
+
+class Z3 implements Comparator{
+	
+	
+	@Override
+	public int compare(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		String s1 = o1.toString();
+		String s2 = o2.toString();
+		
+		return s2.compareTo(s1);
+	}
+}
+
+public class Manager7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		TreeMap tree = new TreeMap(new Z3());
+		
+		tree.put(201, "java");
+		tree.put(101, "java");
+		tree.put(111, "java");
+		tree.put(103, "java");
+		tree.put(109, "java");
+		tree.put(211, null);
+		
+		//tree.put("valu 1", "java");  
+		System.out.println(tree);
+		
+TreeMap tree1 = new TreeMap(new Z3());
+		
+		tree1.put("abc", "java");
+		tree1.put("xyz", "java");
+		tree1.put("qrs", "java");
+		tree1.put("uvw", "java");
+		tree1.put("fga", "java");
+		tree1.put("puff", null);
+		
+		//tree.put("valu 1", "java");  
+		System.out.println(tree1);
+	}
+
+}
